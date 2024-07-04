2185ee3eaa4d3d1a534d97ab75bb405ef6ad5231
WTaylor332 <118070192+WTaylor332@users.noreply.github.com>
2024-07-04 16:48:03 +0100

    Update modelTest.py
    model update

--- modelTest.py
+++ modelTest.py
@@ -1,52 +1,135 @@
-import numpy as np
-import uproot
-from tqdm import tqdm
+import numpy as np 
+print()
+import tensorflow as tf 
+from tensorflow import keras
+from tensorflow.keras.callbacks import TensorBoard
+print()
+import matplotlib.pyplot as plt 
+from matplotlib import cm 
+from accessData import *
+from sklearn.preprocessing import StandardScaler
 
-def loadData(dataFile):
-    f = uproot.open(dataFile+':L1TrackNtuple/eventTree')
-    events = f.arrays(['trk_pt', 'trk_eta','trk_phi','trk_z0','trk_nstub', 'trk_bendchi2','trk_chi2dof','pv_MC','pv_L1reco'])
-    return events['trk_z0'], events['trk_pt'], events['pv_MC']
+
+def loading(name):
+    if name == "TTbar.root":
+        eventZ, eventPT, eventsPV = loadData(name)
+        rawDataAll = rawPaddedData(eventZ, eventPT)
+        binDataAll = histogramData(rawData)
+    else:
+        eventsAll = np.array([[]])
+        eventsPV = np.array([])
+        rawDataAll = np.array([])
+        binDataAll = np.array([])
+
+        for i in range(100,110):
+            name = 'GTTObjects_ttbar200PU_{}.root'.format(i)
+            eventZ, eventPT, pv = loadData(name)
+            print(len(eventZ))
+            print(len(eventPT))
+            rawData = rawPaddedData(eventZ, eventPT)
+            rawDataAll = np.append(rawDataAll, rawData)
+            binData = histogramData(rawData)
+            binDataAll = np.append(binDataAll, binData)
+            pv = np.array(pv).flatten()
+            print(pv.shape)
+            eventsPV = np.append(eventsPV, [pv])
+
+        print()
+        print(eventsAll.shape)
+
+    return rawDataAll, binDataAll, eventsPV.reshape(-1)
 
 
-def rawPaddedData(eventZ, eventPT):
-    padSize = 300
-    eventsSorted = np.array([[]])
+TTbar = np.load("TTbar.npz")
+zRaw, ptRaw, ptBin, pvRaw = TTbar['z'], TTbar['pt'], TTbar['ptB'], TTbar['pv']
 
-    for i in range(len(eventZ)):
-        indexSort = np.argsort(eventZ[i])
-        zSort = np.sort(eventZ[i])
-        padding = np.array([999999 for x in range((padSize-len(zSort)))])
-        zSort = np.append(zSort, padding)
-        ptSort = np.array([eventPT[i][indexSort]])
-        ptSort = np.append(ptSort, padding)
-        eventsSorted = np.append(eventsSorted, [zSort, ptSort])
 
-    eventsSorted = eventsSorted.reshape(len(eventZ),2,padSize)
+def binModel(pt, pv):
 
-    return eventsSorted
+    t = len(pt)//10
+    v = len(pt)//5
+    xTest, xValid, xTrain = pt[:t], pt[t:v], pt[v:]
+    yTest, yValid, yTrain = pv[:t], pv[t:v], pv[v:]
 
-# binning
+    model = keras.models.Sequential([
+        keras.layers.Flatten(input_shape=(300,)),
+        keras.layers.Dense(200, activation="relu"),
+        keras.layers.Dense(100, activation="relu"),
+        keras.layers.Dense(1)
+    ])
 
-def histogramData(rawData):
+    model.summary()
 
-    binnedMatrix = np.zeros((rawData.shape[0], rawData.shape[1], rawData.shape[2]))
-    ptBin = np.zeros(300) + 999999
+    model.compile(optimizer='adam',
+    loss = "mean_absolute_error",
+    metrics=["accuracy"])
 
-    for i in range(rawData.shape[0]):
-        ptBin = np.zeros(300) + 999999
-        binnedMatrix[i][0] = np.linspace(-14.9,15,300).round(2)
-        ptValue = 0
-        binValue = -14.9
-        count = 0 
-        while binValue <= 15:
-            if rawData[i][0][count] <= binValue:
-                ptValue += (rawData[i][1][count])
-                count += 1
-            if rawData[i][0][count] > binValue:
-                index = np.where(binnedMatrix[i][0] == round(binValue,1))
-                ptBin[index] = ptValue
-                binValue += 0.1
-                ptValue = 0
+    history = model.fit(xTrain, yTrain, epochs=10)
 
-        binnedMatrix[i][1] = ptBin
-    return binnedMatrix
+    model.evaluate(xValid, yValid)
+
+    yPredicted = model.predict(xTest)
+    yPredictedLabels = [np.argmax(i) for i in yPredicted]
+    print(yPredicted.shape)
+    print(yPredicted[:5])
+
+    return history
+
+
+
+def rawModel(z, pt, pv):
+    z[z == float("nan")] = 9999
+    pt[pt == float("nan")] = 9999
+    binDataAll = np.stack((z,pt), axis=1)
+    t = len(binDataAll)//10
+    v = len(binDataAll)//5
+    xTest, xValid, xTrain = binDataAll[:t], binDataAll[t:v], binDataAll[v:]
+    yTest, yValid, yTrain = pv[:t], pv[t:v], pv[v:]
+
+    model = keras.models.Sequential([
+        keras.layers.Flatten(input_shape=(2,300)),
+        keras.layers.Dense(200, activation="relu"),
+        keras.layers.Dense(100, activation="relu"),
+        keras.layers.Dense(50, activation="relu"),
+        keras.layers.Dense(25, activation="relu"),
+        keras.layers.Dense(1, activation="softmax")
+    ])
+
+    model.summary()
+
+    model.compile(optimizer='adam',
+    loss = "mean_absolute_error",
+    metrics=["accuracy"])
+
+    history = model.fit(xTrain, yTrain, epochs=5, validation_data=(xValid, yValid))
+
+    return history
+
+
+# hist = binModel(pt, pvRaw.flatten())
+
+# loss = hist.history['loss']
+# val_loss = hist.history['val_loss']
+
+# epochs = range(1, len(loss) + 1)
+
+# plt.plot(epochs, loss, 'bo', label='Training Loss')
+# plt.plot(epochs, val_loss, 'b', color='red', label='Validation Loss')
+# plt.title('Training and Validation Loss')
+# plt.legend()
+# plt.savefig("binModel_loss2.png")
+
+
+# print()
+# hist = rawModel(zRaw, ptRaw, pvRaw)
+
+# loss = hist.history['loss']
+# val_loss = hist.history['val_loss']
+
+# epochs = range(1, len(loss) + 1)
+
+# plt.plot(epochs, loss, 'bo', label='Training Loss')
+# plt.plot(epochs, val_loss, 'b', color='red', label='Validation Loss')
+# plt.title('Training and Validation Loss')
+# plt.legend()
+# plt.savefig("rawModel_loss1.png")
