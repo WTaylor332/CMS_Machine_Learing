84252b9e1edcad2f1ef3f2e5603c1617222cda4d
WTaylor332 <118070192+WTaylor332@users.noreply.github.com>
2024-07-11 09:20:12 +0100

    Update accessData.py
    extracting track number and putting it into bins

--- accessData.py
+++ accessData.py
@@ -2,6 +2,7 @@ import numpy as np
 import uproot
 from tqdm import tqdm
 from sklearn.preprocessing import StandardScaler
+from tqdm import tqdm
 
 def loadData(dataFile):
     f = uproot.open(dataFile+':L1TrackNtuple/eventTree')
@@ -9,63 +10,120 @@ def loadData(dataFile):
     return events['trk_z0'], events['trk_pt'], events['pv_MC']
   
 
+def modifyData(z, pt, ptCut=2):
+
+    pt = np.where(pt<2, np.nan, pt)
+    index = np.argwhere(np.isnan(pt))
+
+    infIndex = np.argwhere(np.isinf(pt))
+    pt[infIndex] = np.nan
+
+    index = np.array(index).flatten()
+    z[index] = np.nan
+    z[infIndex] = np.nan
+
+    return z, pt
+
+
 def rawPaddedData(eventZ, eventPT):
-    padSize = 300
-    eventZScaled = np.array([[]])
-    eventPTScaled = np.array([[]])
-    for i in range(len(eventZ)):
-        indexSort = np.argsort(eventZ[i])
-        zSort = np.sort(eventZ[i])
-        padding = np.array([np.nan for x in range((padSize-len(zSort)))])
-        zSort = np.append(zSort, padding)
-        ptSort = np.array([eventPT[i][indexSort]])
-        ptSort = np.append(ptSort, padding)
-        eventZScaled = np.append(eventZScaled, [zSort])
-        eventPTScaled = np.append(eventPTScaled, [ptSort])
+    padSize = 400
+    eventZData = np.zeros((len(eventZ), padSize))
+    eventPTData = np.zeros((len(eventPT), padSize))
+    trackLength = np.zeros(len(eventZ))
+    maxTLength = 0
+    for i in tqdm(range(len(eventZ))):
 
-    arr = np.stack((eventZScaled, eventPTScaled), axis=1)
+        zSort = np.zeros(padSize)
+        padding = np.array([np.nan for x in range((padSize-len(eventZ[i])))])
+        zSort[:len(eventZ[i])] = eventZ[i]
+        zSort[len(eventZ[i]):] = padding
 
+        ptSort = np.zeros(padSize)
+        ptSort[:len(eventPT[i])] = eventPT[i]
+        ptSort[len(eventPT[i]):] = padding
+
+        z, pt = modifyData(zSort, ptSort)
+
+        indexSort = np.argsort(z)
+        zSort = np.sort(z)
+        ptSort = pt[indexSort]
+
+        if np.argwhere(np.isnan(zSort))[0][0] > maxTLength:
+            maxTLength = np.argwhere(np.isnan(zSort))[0][0]
+            trackLength[i] = np.argwhere(np.isnan(zSort))[0][0]
+
+        eventZData[i] = zSort
+        eventPTData[i] = ptSort
+
+    # to scale pt data
+    zFlat = eventZData.flatten()
+    ptFlat = eventPTData.flatten()
+    arr = np.stack((zFlat, ptFlat), axis=1)
+    print(arr.shape)
     scaler = StandardScaler().fit(arr)
     arrScaled = scaler.transform(arr)
-    eventPTScaled = arrScaled[:,1].reshape(len(eventPT), padSize)
-
-    return eventZScaled.reshape(1360,300), eventPTScaled
-
+    eventPTData = arrScaled[:,1].reshape(len(eventPT), padSize)
+    eventZData = eventZData[:, :maxTLength]
+    eventPTData = eventPTData[:, :maxTLength]
+    print(eventZData.shape, eventPTData.shape)
+    return eventZData, eventPTData, trackLength, maxTLength
 
 
 # binning
 
 def histogramData(z, pt):
-    binnedMatrix = np.zeros((z.shape[0], z.shape[1]))
-
-    for i in range(z.shape[0]):
+    ptBinnedMatrix = np.zeros((z.shape[0], 300))
+    trackBinnedMatrix = np.zeros((z.shape[0], 300))
+    for i in tqdm(range(z.shape[0])):
         ptBin = np.zeros(300)
         binValueMatrix = np.linspace(-14.9,15,300).round(2)
         ptValue = 0
         binValue = -14.9
         count = 0 
-        while binValue <= 15:
+        tracksBin = np.zeros(300)
+        trackCount = 0
+        while binValue <= 15 and count < len(z[i]):
+
             if z[i][count] <= binValue:
                 ptValue += (pt[i][count])
                 count += 1
+                trackCount += 1
             else:
-                index = np.where(binValueMatrix == round(binValue,1))
+                index = np.where(binValueMatrix == round(binValue,1))[0]
                 ptBin[index] = ptValue
                 binValue += 0.1
                 ptValue = 0
+                tracksBin[index] = trackCount
+                trackCount = 0
 
-        binnedMatrix[i] = ptBin
-    return binnedMatrix
+        ptBinnedMatrix[i] = ptBin
+        trackBinnedMatrix[i] = tracksBin
+    print(trackBinnedMatrix[:2])
+    print(ptBinnedMatrix[:2])
+    return ptBinnedMatrix, trackBinnedMatrix
+
+# name = "TTbar.root"
+# n = "GTTObjects_ttbar200PU_1.root"
+# eventZ,  eventPT, eventPV = loadData(name)
+
+# zRaw, ptRaw, trackLength, mv = rawPaddedData(eventZ, eventPT)
+
+# # print()
+# # print(zRaw[0])
+# # print(ptRaw[0])
+
+# mv = np.array([mv])
+
+# np.savez('TTbarRaw3', z=zRaw, pt=ptRaw, pv=np.array(eventPV), tl=trackLength, maxValue=mv)
+
+rawD = np.load('TTbarRaw3.npz')
+zRaw, ptRaw = rawD['z'], rawD['pt']
+ptBin, trackBin = histogramData(zRaw, ptRaw)
+
+np.savez('TTbarBin4', ptB=ptBin, tB=trackBin)
+
+q = np.load('TTbarBin4.npz')
+print(q['ptB'].shape)
+print(q['tB'].shape)
 
 
-name = "TTbar.root"
-n = "GTTObjects_ttbar200PU_1.root"
-eventZ,  eventPT, eventPV = loadData(name)
-print("s")
-print(len(eventZ), len(eventPT), len(eventPV))
-print(len(eventZ[0]), len(eventPT[0]), len(eventPV[0]))
-zRaw, ptRaw = rawPaddedData(eventZ, eventPT)
-print("w")
-ptBin = histogramData(zRaw, ptRaw)
-
-np.savez('TTbar', z=zRaw, pt=ptRaw, ptB=ptBin, pv=np.array(eventPV))
