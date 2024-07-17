c9613e5d3e80b0e35191cdf4690c02ea86f2afd0
Walter Taylor <bgz16927@mercury009.pp.rl.ac.uk>
2024-07-17 16:41:29 +0100

    reading eta values from ttbar also

--- accessData.py
+++ accessData.py
@@ -7,10 +7,10 @@ from tqdm import tqdm
 def loadData(dataFile):
     f = uproot.open(dataFile+':L1TrackNtuple/eventTree')
     events = f.arrays(['trk_pt', 'trk_eta','trk_phi','trk_z0','trk_nstub', 'trk_bendchi2','trk_chi2dof','pv_MC','pv_L1reco'])
-    return events['trk_z0'], events['trk_pt'], events['pv_MC']
+    return events['trk_z0'], events['trk_pt'], events['pv_MC'], events['trk_eta']
   
 
-def modifyData(z, pt, ptCut=2):
+def modifyData(z, pt, eta, ptCut=2):
 
     pt = np.where(pt<2, np.nan, pt)
     index = np.argwhere(np.isnan(pt))
@@ -22,13 +22,18 @@ def modifyData(z, pt, ptCut=2):
     z[index] = np.nan
     z[infIndex] = np.nan
 
-    return z, pt
+    index = np.array(index).flatten()
+    eta[index] = np.nan
+    eta[infIndex] = np.nan
+
+    return z, pt, eta
 
 
-def rawPaddedData(eventZ, eventPT):
+def rawPaddedData(eventZ, eventPT, eventEta):
     padSize = 400
     eventZData = np.zeros((len(eventZ), padSize))
     eventPTData = np.zeros((len(eventPT), padSize))
+    eventEtaData = np.zeros((len(eventEta), padSize))
     trackLength = np.zeros(len(eventZ))
     maxTLength = 0
     for i in tqdm(range(len(eventZ))):
@@ -42,11 +47,16 @@ def rawPaddedData(eventZ, eventPT):
         ptSort[:len(eventPT[i])] = eventPT[i]
         ptSort[len(eventPT[i]):] = padding
 
-        z, pt = modifyData(zSort, ptSort)
+        etaSort = np.zeros(padSize)
+        etaSort[:len(eventEta[i])] = eventEta[i]
+        etaSort[len(eventEta[i]):] = padding
+
+        z, pt, eta = modifyData(zSort, ptSort, etaSort)
 
         indexSort = np.argsort(z)
         zSort = np.sort(z)
         ptSort = pt[indexSort]
+        etaSort = eta[indexSort]
 
         if np.argwhere(np.isnan(zSort))[0][0] > maxTLength:
             maxTLength = np.argwhere(np.isnan(zSort))[0][0]
@@ -54,19 +64,23 @@ def rawPaddedData(eventZ, eventPT):
 
         eventZData[i] = zSort
         eventPTData[i] = ptSort
+        eventEtaData[i] = etaSort
 
     # to scale pt data
     zFlat = eventZData.flatten()
     ptFlat = eventPTData.flatten()
-    arr = np.stack((zFlat, ptFlat), axis=1)
+    etaFlat = eventEtaData.flatten()
+    arr = np.stack((zFlat, ptFlat, etaFlat), axis=1)
     print(arr.shape)
     scaler = StandardScaler().fit(arr)
     arrScaled = scaler.transform(arr)
     eventPTData = arrScaled[:,1].reshape(len(eventPT), padSize)
+    eventEtaData = arrScaled[:,2].reshape(len(eventEta), padSize)
     eventZData = eventZData[:, :maxTLength]
     eventPTData = eventPTData[:, :maxTLength]
-    print(eventZData.shape, eventPTData.shape)
-    return eventZData, eventPTData, trackLength, maxTLength
+    eventEtaData = eventEtaData[:, :maxTLength]
+    print(eventZData.shape, eventPTData.shape, eventEtaData.shape)
+    return eventZData, eventPTData, eventEtaData, trackLength, maxTLength
 
 
 # binning
@@ -102,23 +116,29 @@ def histogramData(z, pt):
     print(ptBinnedMatrix[:2])
     return ptBinnedMatrix, trackBinnedMatrix
 
-name = "QCD_Pt-15To3000.root"
-eventZ,  eventPT, eventPV = loadData(name)
+# -------------------------------------------------------------------------------------------------------------------------------------------
+# -----------------------------------------------------------------MAIN----------------------------------------------------------------------
+# -------------------------------------------------------------------------------------------------------------------------------------------
 
-zRaw, ptRaw, trackLength, mv = rawPaddedData(eventZ, eventPT)
+name = "TTbar.root"
+eventZ,  eventPT, eventPV, eventEta = loadData(name)
 
-# # print()
-# # print(zRaw[0])
-# # print(ptRaw[0])
+zRaw, ptRaw, etaRaw, trackLength, mv = rawPaddedData(eventZ, eventPT, eventEta)
 
-np.savez('QCD_Pt-15To3000', z=zRaw, pt=ptRaw, pv=np.array(eventPV), tl=trackLength, maxValue=np.array([mv]))
+print()
+print(zRaw[0])
+print(ptRaw[0])
+print(etaRaw[0])
 
+np.savez('TTbarRaw5', z=zRaw, pt=ptRaw, eta=etaRaw, pv=np.array(eventPV), tl=trackLength, maxValue=np.array([mv]))
 
-rawD = np.load('QCD_Pt-15To3000.npz')
-zRaw, ptRaw = rawD['z'], rawD['pt']
+rawD = np.load('TTbarRaw5.npz')
+zRaw, ptRaw, etaRaw = rawD['z'], rawD['pt'], rawD['eta']
 t = rawD['tl']
 m = rawD['maxValue']
-print(zRaw[0], ptRaw[0], t[0], m)
+print(zRaw[0], ptRaw[0], etaRaw[0], t[0], m)
+
+
 # ptBin, trackBin = histogramData(zRaw, ptRaw)
 
 # np.savez('TTbarBin4', ptB=ptBin, tB=trackBin)
