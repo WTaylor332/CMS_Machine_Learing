7d4756483bf2412dcd84e41091264d94913e3e34
Walter Taylor <bgz16927@mercury011.pp.rl.ac.uk>
2024-07-18 16:15:39 +0100

    wavent changing dilations rate

--- model_types.py
+++ model_types.py
@@ -281,7 +281,7 @@ def rnn(form, op, lossFunc):
     return model
     
 
-def wavenet(form):
+def wavenet(form, op, lossFunc):
     if form[1] < 2:
         #1D model
         model = keras.models.Sequential([
@@ -299,19 +299,41 @@ def wavenet(form):
         ])
     else:
         # 2D model
+        # model = keras.models.Sequential([
+        #     # convolutional layers
+        #     keras.layers.Conv2D(12, kernel_size=(8,1), activation='relu', input_shape=(form), dilation_rate=(2,2)),
+        #     keras.layers.MaxPool2D(pool_size=(4,4)),
+
+        #     # multi later perceptron
+        #     keras.layers.Flatten(),
+        #     keras.layers.Dense(15, activation="relu"),
+        #     # keras.layers.Dropout(rate=0.3),
+        #     keras.layers.Dense(5, activation="relu"),
+        #     # keras.layers.Dropout(rate=0.3),
+        #     keras.layers.Dense(1)
+        # ])
         model = keras.models.Sequential([
             # convolutional layers
-            keras.layers.Conv2D(12, kernel_size=(8,1), activation='relu', input_shape=(form), dilation_rate=(2,2)),
-            keras.layers.MaxPool2D(pool_size=(4,4)),
+            keras.layers.Conv2D(12, kernel_size=(1,8), activation='relu', input_shape=(form), dilation_rate=(1,2)),
+            keras.layers.MaxPool2D(pool_size=(1,4)),
+
+            keras.layers.Conv2D(12, kernel_size=(1,8), activation='relu', dilation_rate=(1,2)),
+            keras.layers.MaxPool2D(pool_size=(1,4)),
+
+            keras.layers.Conv2D(12, kernel_size=(1,8), activation='relu', dilation_rate=(1,2)),
+            keras.layers.MaxPool2D(pool_size=(1,2)),
 
             # multi later perceptron
             keras.layers.Flatten(),
             keras.layers.Dense(15, activation="relu"),
             # keras.layers.Dropout(rate=0.3),
+            keras.layers.Dense(10, activation="relu"),
+            # keras.layers.Dropout(rate=0.3),
             keras.layers.Dense(5, activation="relu"),
             # keras.layers.Dropout(rate=0.3),
             keras.layers.Dense(1)
         ])
+    model.compile(optimizer=op, loss=lossFunc)
     return model
 
 
