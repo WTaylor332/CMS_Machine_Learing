c200f80a11e4ee4360373c827e739ccb4d0f669f
Walter Taylor <bgz16927@mercury011.pp.rl.ac.uk>
2024-08-05 16:03:59 +0100

    getting probability for ttbar

--- accessData.py
+++ accessData.py
@@ -241,7 +241,7 @@ def percentageHardVertex(z, pv, bins=30):
 # zRaw, ptRaw, etaRaw, trackLength, mv = rawPaddedData(eventZ, eventPT, eventEta)
 # np.savez('TTbarRaw5', z=zRaw, pt=ptRaw, eta=etaRaw, pv=np.array(eventPV), tl=trackLength, maxValue=np.array([mv]))
 
-# rawD = np.load('QCD_Pt-15To3000.npz')
+rawD = np.load('TTbarRaw5.npz')
 # zRaw, ptRaw, etaRaw = rawD['z'], rawD['pt'], rawD['eta']
 # t = rawD['tl']
 # m = rawD['maxValue']
@@ -259,7 +259,7 @@ def percentageHardVertex(z, pv, bins=30):
 
 # zMerge, ptMerge, etaMerge, pvMerge, trackLength = merge()
 # np.savez('Merged_deacys_Raw', z=zMerge, pt=ptMerge, eta=etaMerge, pv=np.array(pvMerge), tl=trackLength)
-mergeData = np.load('Merged_deacys_Raw.npz')
+# mergeData = np.load('Merged_deacys_Raw.npz')
 # z, pt, eta = mergeData['z'], mergeData['pt'], mergeData['eta']
 # print()
 # print(z.shape, pt.shape, eta.shape)
@@ -275,5 +275,6 @@ mergeData = np.load('Merged_deacys_Raw.npz')
 
 # adding probability of hard vertext to mixed data
 b = 30
-vertProb, vertBin = percentageHardVertex(mergeData['z'], mergeData['pv'], b)
-np.savez(f'Hard_Vertex_Probaility_{b}_bins', prob=vertProb, bins=vertBin)
+vertProb, vertBin = percentageHardVertex(rawD['z'], rawD['pv'], b)
+np.savez(f'TTbar_Hard_Vertex_Probaility_{b}_bins', prob=vertProb, bins=vertBin)
+
